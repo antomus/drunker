badef409dab85a831c2364c2899b54a32d528675
antomus <antomus81@gmail.com>
2021-09-07 09:13:04 +0300

    Initial commit

---
+++ DrunkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okten.drunker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrunkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
