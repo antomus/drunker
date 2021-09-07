46a4c3aaf2135280e364fd919e07b9311c572360
antomus <antomus81@gmail.com>
2021-09-07 11:13:07 +0300

    add maven project files

---
+++ src/main/java/com/okten/drunker/DrunkerApplication.java
@@ -0,0 +1,13 @@
+package com.okten.drunker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrunkerApplication.class, args);
+	}
+
+}
